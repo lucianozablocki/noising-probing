4e1ddfcac1aba0ffc9f16d7dc6c3a6dfa14f5e31
lzablocki <lucianozablocki@gmail.com>
2025-07-18 07:36:03 -0300

    take maver rows as reducer
    Signed-off-by: lzablocki <lucianozablocki@gmail.com>

--- model.py
+++ model.py
@@ -170,9 +170,9 @@ class SecondaryStructurePredictor(nn.Module):
         x_2d = self.resnet(x_2d)
 
         # Probing prediction branch
-        x_2d_mean = torch.mean(x_2d, 2) # rows
-        x_2d_mean = x_2d_mean.permute(0, 2, 1)
-        probing_pred = self.probing_predictor_2(x_2d_mean)
+        x_2d_max, _ = torch.max(x_2d, 2) # max over rows
+        x_2d_max = x_2d_max.permute(0, 2, 1)
+        probing_pred = self.probing_predictor_2(x_2d_max)
 
         # Contact prediction branch
         x_2d = self.conv_out(x_2d)
